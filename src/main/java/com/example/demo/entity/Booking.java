b084f1f7a3230b536b1a334ed0f9664fc0b76605
23eg104e22-boop <23eg104e22@anurag.edu.in>
2026-04-03 21:46:35 +0530

    final project

---
+++ src/main/java/com/example/demo/entity/Booking.java
@@ -0,0 +1,61 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long resourceId;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    private String status;
+
+    public Booking() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(Long resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
